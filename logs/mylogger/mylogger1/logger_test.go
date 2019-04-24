77903b152a975183f631e2c68ffb566e3143c0c0
rangeldaykes <daykes@gmail.com>
2019-04-24 11:00:00 -0300

    console logger

--- logs/mylogger/mylogger1/logger_test.go
+++ logs/mylogger/mylogger1/logger_test.go
@@ -8,7 +8,8 @@ import (
 )
 
 func TestLogger(t *testing.T) {
-	//mylogger1.NewLogger(mylogger1.NewFileLogger("Logteste", ""), mylogger1.LEVEL_LOG)
+
+	//log := mylogger1.NewLogger(mylogger1.NewConsoleLoger(), mylogger1.LEVEL_LOG)
 
 	log := mylogger1.NewLogger(
 		mylogger1.NewFileLogger(
