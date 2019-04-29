25f9d0c18a415d4ed10453644557860fa6f9f28a
rangeldaykes <daykes@gmail.com>
2019-04-29 16:48:14 -0300

    io.reader io.writer

---
+++ ioreader/ioreader1/ioreader1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	r := strings.NewReader("abcde")
+
+	buf := make([]byte, 4)
+	for {
+		n, err := r.Read(buf)
+		fmt.Println(n, err, buf[:n])
+		if err == io.EOF {
+			break
+		}
+	}
+}
